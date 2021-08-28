07ab75eae728b1b262842494754861454a0efdbd
Piotr <piotr.w.miecz@gmail.com>
2021-08-28 03:27:28 +0200

    aded heroku middleware whitenoise

--- hum_temp/settings.py
+++ hum_temp/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # added
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # original
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
